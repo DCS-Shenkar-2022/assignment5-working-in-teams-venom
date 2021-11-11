9973d425bcd903b8f67ed0b1ef09c02bc18eb46a
mhmd atamny <mhmdatamny@h-MacBook-Air-sl-mhmd.local>
2021-11-11 20:50:41 +0200

    m

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8080;
 
 http.createServer((req, res) => {
     res.writeHead(200); // Status code in header
-    res.write("Linoy Siman Tov"); // Send res body (can write multiple times)
+    res.write("Muhammed Otmani"); // Send res body (can write multiple times)
     res.end(); // Send & close connection
 }).listen(port); // Listen for connection on this port
 
