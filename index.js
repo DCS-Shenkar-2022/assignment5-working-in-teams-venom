9f871683eef84e8e169e938ab3218fba65bcaa98
NoyShabo <Noysh1234@gmail.com>
2021-11-16 16:02:46 +0200

    adding params check

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
 
-const express= require("express");
-const app= express();
-const port = process.env.PORT||8080;
 
+app.param('githubUserName', (req, res, next, value) => {
+    const regex = /^[a-zA-Z]+$/g;
+    const checkParam = regex.test(req.params.githubUserName);
+    next();
+});
 
-app.get('/api/v1/githubUser/:githubUserName',(req,res)=> {
+app.get('/api/v1/githubUser/:githubUserName', (req, res) => {
     res.send(`Connecting to:${req.params.githubUserName}`);
-    console.log(`Server ready at http://localhost:${port}/api/v1/githubUser/${req.params.githubUserName}`);
-    
 });
 
-app.get('/api/v1/githubUser/:githubUserName/avatar',(req,res)=> {
+app.get('/api/v1/githubUser/:githubUserName/avatar', (req, res) => {
     res.send(`${req.params.githubUserName}`);
-    console.log(`Server ready at http://localhost:${port}`);
 });
 
-app.get('/api/v1/githubUser/:githubUserName/repo/:repoName',(req,res)=> {
+
+
+app.param('repoName', (req, res, next, value) => {
+    const regex = /^[a-zA-Z]+$/g;
+    const checkParam = regex.test(req.params.repoName);
+    next();
+});
+
+app.get('/api/v1/githubUser/:githubUserName/repo/:repoName', (req, res) => {
     res.send(` ${req.params.githubUserName} / repo/ ${req.params.repoName}`);
-    console.log(`Server ready at http://localhost:${port}`);
 });
 
-app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers',(req,res)=> {
-    res.send(` ${req.params.githubUserName} / repo/ ${req.params.repoName} and contributers: ${contributers}`);
-    console.log(`Server ready at http://localhost:${port}`);
+app.get('/api/v1/githubUser/:githubUserName/repo/:repoName/contributers', (req, res) => {
+    res.send(` ${req.params.githubUserName} / repo/ ${req.params.repoName}`);
 });
 
-app.all('*', (req,res) => res.send('Global handler forall routes'));
+app.all('*', (req, res) => res.send('Global handler forall routes'));
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server ready at http://localhost:${port}`);
 });
 
 console.log(`Listening on port ${port}`);
-
- 
