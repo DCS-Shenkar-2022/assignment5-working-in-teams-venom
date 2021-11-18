d9b4f94ca39a6b99a714e73f7d2d1df5873aceb8
RotemEzra1 <rotem19961@gmail.com>
2021-11-18 16:08:28 +0200

    ex5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getUser } = require("./getApi");
+const { getUser, getRepo } = require("./getApi");
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -16,10 +16,17 @@ app.get('/api/v1/githubUser/:githubUserName', (req, res) => {
     res.send(`Connecting to:${req.params.githubUserName}`);
 });
 
-app.get('/api/v1/githubUser/:githubUserName/avatar', (req, res) => {
-    res.send(`${req.params.githubUserName}`);
-});
 
+
+app.get('/api/v1/githubUser/:githubUserName/:repoName', (req, res) => {
+    getRepo(req.params.githubUserName, req.params.repoName)
+        .then(data => {
+            console.log(data);
+            res.send(data);
+        }).catch(() => {
+            res.send("Repo Not Exist");
+        });
+});
 
 app.param('repoName', (req, res, next, value) => {
     const regex = /^[a-zA-Z]+$/g;
